932616bc93df3a14161632b91751fc8eb3f27563
Davi Machado <davirobergemachado@gmail.com>
2023-08-25 18:24:28 +0000

    #63

--- cliente/js/cena-lagoa.js
+++ cliente/js/cena-lagoa.js
@@ -4,14 +4,33 @@ export default class lagoa extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('tela-aberturav', '../assets/telaabertura.png')
+    this.load.image('grama', '../assets/cenarios/grama_floresta.png')
+    this.load.spritesheet('thiaguinho', '../assets/patos/thiaguinho/thiaguinho-default-walking.png', {
+      frameWidth: 64,
+      frameHeight: 68
+    })
   }
 
   create () {
     this.imagem = this.add
-      .image(225, 400, 'tela-abertura')
-      .setInteractive()
+      .image(400, 400, 'grama')
+    this.personagem = this.physics.add.sprite(225, 400, 'thiaguinho')
+    .setInteractive()
+    .on('pointerdown', () => {
+      this.personagem.anims.play('pato-direita')
+      this.personagem.setVelocityX(50)
+    })
+
+    this.anims.create({
+      key: 'pato-direita',
+      frames: this.anims.generateFrameNumbers('thiaguinho', {
+        start: 0,
+        end: 21
+      }),
+      frameRate: 32,
+      repeat: -1
+    })
   }
 
-  update () { }
+update() { }
 }
